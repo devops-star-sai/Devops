38980861a498b69c01817ff5bf46dc257e9bad66
devops-star-sai <68603310+devops-star-sai@users.noreply.github.com>
2020-07-23 01:10:38 +0530

    Update addition.py

--- python/addition.py
+++ python/addition.py
@@ -1,16 +1,16 @@
-#x = int(input("enter value of x"))
-#y = int(input ("enter value of y"))
+a = int(input("enter value of x"))
+b = int(input ("enter value of y"))
 #x=30
 #y=35
-def add(x,y):
-	print("the value of x is " + str(x))
-	print("the value of y is " + str(y))
+def add(a,b):
+	print("the value of x is " + str(a))
+	print("the value of y is " + str(b))
 	result = x * y
 	print(result)
-add(x,y)
+add(a,b)
 
 
-z = x+y
+#z = x+y
 print( "the value of x is " ,x)
 print( " the value of y is " ,y)
 print(z)
